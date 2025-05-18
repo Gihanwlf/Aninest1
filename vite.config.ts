f9e24f07d29c0bfc07ed84172e69474ed9d08abf
Gihanwlf <pgihan29@gmail.com>
2025-05-18 10:50:54 +0530

    update 67

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   base: '/Aninest1/', 
+   base: 'https://gihanwlf.github.io/Aninest1/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
